42e65d9f2ee0e60c324f9209c3b8e10d534af468
Mahdi-s <mahdi.sv98@gmail.com>
2024-05-12 19:26:59 -0500

    Update hero.tsx

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -69,8 +69,8 @@ export function BackgroundBoxesDemo() {
                 style={{ wordWrap: "break-word" }}
               >
                 
-                Hey! Welcome to my corner of the internet. I have a passion for <strong>machine learning</strong>, creating <strong>analytic dashboards</strong>, and
-                conducting <strong>simulations</strong>. This page showcases a sample of my
+                Hey! Welcome to my corner of the internet. I like to research <strong>machine learning</strong>, create easy to use<strong>analytic dashboards</strong>, and
+                conduct <strong>simulations</strong> on human interactions. This page showcases a sample of my
                 work and interests. Feel free to reach out if you have any
                 questions or just want to chat.
                 
