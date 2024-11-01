f06834a45019aa9b4307eac3c14c7d2347e2f81e
mahdi-s <mahdi.sv98@gmail.com>
2024-11-01 16:10:11 -0500

    phone view bug and performance

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        return config;
+    },
+    experimental: {
+        outputFileTracing: true,
+    },
+    webSocketTimeout: 30000,
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+    },
+};
 
 export default nextConfig;
